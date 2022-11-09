4590b56963958bb037d0e4f00206cbfc8bbcdba1
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-11-09 20:52:26 -0300

    feat: added code config

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 const app = express();
 
+interface Teste{}
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    }
-);
- export default app;
+});
+export default app;
